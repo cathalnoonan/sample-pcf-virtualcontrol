c9d54597b59d267bb2e1c5c043ab4d24f5b4990b
Cathal Noonan <14317886+cathalnoonan@users.noreply.github.com>
2021-12-19 19:51:54 +0000

    Resolve control width issue (force containing DIV to have 100% width)

--- control/TextControl/TextControlReactClassComponent.tsx
+++ control/TextControl/TextControlReactClassComponent.tsx
@@ -1,17 +1,21 @@
 import * as React from 'react';
 
 export interface TextControlReactProps {
-  value: string | null
+  value: string | null;
   onChange: (newValue: string) => void
 }
 
 export interface TextControlReactState {
-  value: string | null
+  value: string | null;
 }
 
 export class TextControlReact extends React.Component<TextControlReactProps, TextControlReactState> {
+  // 'ref' is needed to set the width of the container DIV to 100%
+  private ref: React.RefObject<HTMLInputElement>
+
   constructor(props: TextControlReactProps) {
     super(props)
+    this.ref = React.createRef<HTMLInputElement>()
     this.state = {
       value: props.value
     }
@@ -20,11 +24,17 @@ export class TextControlReact extends React.Component<TextControlReactProps, Tex
   public render(): JSX.Element {
     return (
         <input 
+          ref={this.ref}
           value={this.state.value ?? ''} 
           placeholder='---' 
           onChange={e => this.onChange(e.currentTarget.value)} 
           className='TextControl' />
     )
+  }
+
+  public componentDidMount() {
+    // Use the 'ref' to set the parent DIV's width to 100%
+    this.ref?.current?.parentElement?.style && (this.ref.current.parentElement.style.width = '100%')
   }
 
   private onChange(newValue: string): void {
--- control/TextControl/TextControlReactFunctionComponent.tsx
+++ control/TextControl/TextControlReactFunctionComponent.tsx
@@ -1,13 +1,20 @@
 import * as React from 'react';
 
 export interface TextControlReactProps {
-  value: string | null
+  value: string | null;
   onChange: (newValue: string) => void
 }
 
 export function TextControlReact(props: TextControlReactProps): JSX.Element {
   const [value, setValue] = React.useState(props.value)
   
+  // Use a ref to get access to the DIV containing the React control.
+  // Do this to set thw width of the DIV to '100%', otherwise the width of the DIV gets precedence over our control.
+  const ref = React.createRef<HTMLInputElement>()
+  React.useEffect(() => {
+    ref.current?.parentElement?.style && (ref.current.parentElement.style.width = '100%')
+  }, [])
+
   function onChange(newValue: string): void {
     setValue(newValue)
     props.onChange(newValue)
@@ -15,6 +22,7 @@ export function TextControlReact(props: TextControlReactProps): JSX.Element {
 
   return (
     <input 
+      ref={ref} 
       value={value ?? ''} 
       placeholder='---' 
       onChange={e => onChange(e.currentTarget.value)} 
